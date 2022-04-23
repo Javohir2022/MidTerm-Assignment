d6b45830a2b6ac97d863ea2876d1e124d8a6d821
Javohir2022 <javohirazodov0107@gmail.com>
2022-04-24 01:54:02 +0900

    first task of Assignment

---
+++ server_client.cpp
@@ -0,0 +1,49 @@
+#include "ros/ros.h"
+#include "Assignment/LocationCheck.h"
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "Location_Check_service_client_node");
+
+  ros::NodeHandle node_handle;
+
+  ros::ServiceClient client = node_handle.serviceClient<Assignment::LocationCheck>("Location_Check");
+
+  Assignment::LocationCheck Location_Check_srv;
+
+  int input_num;
+
+  std::cout << "Type '0123' if you want to quit" << std::endl;
+
+  while (ros::ok())
+  {
+    std::cout << "Enter a number: ";
+    std::cin >> input_num
+    
+
+    if (input_num < 10)
+    {
+      ROS_INFO("Sending the location...");
+      return 0;
+    }
+
+    if (client.call(Location_Check_srv))
+    {
+      std::string resp;
+      resp = Location_Check_srv.response.answer;
+
+      std::cout << "It is sunny " << resp << std::endl;
+    }
+    else
+    {
+      ROS_ERROR("Service Call Failed");
+      return 1;
+    }
+
+    std::cin.clear();
+
+
+  }
+}
